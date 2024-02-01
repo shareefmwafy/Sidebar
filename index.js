d2bb77102be6875da6c872c20e48f87ff97ba0f3
shareef <s12029160@stu.najah.edu>
2024-02-02 00:42:56 +0200

    finish Operation

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const overlay = document.querySelector(".overlay");
 const buttonClose =document.querySelector(".buttonClose");
 buttonshow.onclick = function() {
 
-    sidebar.classList.toggle("show-sidebar");
+    sidebar.classList.add("show-sidebar");
     overlay.classList.toggle("d-none");
+    sidebar.classList.remove("close-sidebar");
     // buttonClose.classList.toggle("close-sidebar");
 }
 buttonClose.onclick = function() {
-    sidebar.classList.toggle("show-sidebar");
+
+    sidebar.classList.remove("show-sidebar");
+    sidebar.classList.add("close-sidebar");
     overlay.classList.toggle("d-none");
 
 }
